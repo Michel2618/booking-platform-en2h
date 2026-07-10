9906e85f72f7044fc0db93f615760edf5f09b72c
Michel Ruwishka <michelruwishka@gmail.com>
2026-07-10 23:53:42 +0530

    Implement Services API CRUD operations and updated the DTO

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ServicesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/services/dto/create-service.dto.ts
@@ -0,0 +1,7 @@
+export class CreateServiceDto {
+  title: string;
+  description: string;
+  duration: number;
+  price: number;
+  isActive?: boolean;
+}
