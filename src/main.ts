81a4b7414dd4b4db0eea52c0ee1e05313737a16c
Michel Ruwishka <michelruwishka@gmail.com>
2026-07-11 17:11:59 +0530

    Add Swagger module for testing and documentations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger webpage web blobk
+  const config = new DocumentBuilder()
+    .setTitle('Booking API')
+    .setDescription('Backend API for managing services and customer bookings')
+    .setVersion('1.0')
+    .addBearerAuth() 
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); 
+
+  await app.listen(3000);
 }
 bootstrap();
